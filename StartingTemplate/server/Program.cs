671d868d1de20eff7c7f931a2a1ab3c07275c3d7
Devin <devinsevy33@gmail.com>
2026-01-23 15:59:48 -0700

    Starting Template Added

---
+++ StartingTemplate/server/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+// using Server.Data; // Uncomment when Data folder is populated
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Register DbContext (Uncomment when PaddleContext is created)
+// builder.Services.AddDbContext<PaddleContext>(options =>
+//    options.UseNpgsql(connectionString));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapControllers();
+
+app.Run();
